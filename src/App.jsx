c9fec6fcf6579ad940afb6c97e4840f7e94b3a86
Rahidul Islam <rahiseli@outlook.com>
2025-04-14 14:32:49 +0600

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline text-amber-600">Hello Tic Tac Toe</h1>
+     <p className="text-2xl text-green-600">Hello world</p>
+    </>
+  )
+}
+
+export default App
