3ba4abe9a11cac90f06d0b98f408ed30e25445b5
Rahidul Islam <rahiseli@outlook.com>
2025-04-15 17:21:09 +0600

    useState is used for traking squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-
-
-
-function App() {
-
-
+import { useState } from 'react';
+function Square({value}) {
+  function handleClick() {
+    
+    
+  }
   return (
-    <>
-     <h1 className="text-3xl font-bold underline text-amber-600">Hello Tic Tac Toe</h1>
-     <p className="text-2xl text-green-600">Hello world</p>
-    </>
-  )
+    <button className="bg-white border border-gray-400 w-12 h-12 text-lg m-1 leading-1" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
-export default App
+function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  console.log(squares)
+  return (
+    <>
+      <div className="flex">
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
+      </div>
+      <div className="flex">
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
+      </div>
+      <div className="flex">
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
