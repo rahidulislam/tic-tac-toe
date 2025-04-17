d13829fe60cfdc523b9eb423077ebb1825f35b51
Rahidul Islam <rahiseli@outlook.com>
2025-04-17 15:43:56 +0600

    tic tac toe is done

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,8 @@ function Square({value,squareClick}) {
   );
 }
 
-function App() {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setxIsNext] = useState(true)
+function Board({xIsNext,squares,onPlay}) {
+  
   const winner = calculateWinner(squares)
   let status;
   if(winner){
@@ -30,8 +29,7 @@ function App() {
       nextSquares[i] = 'O'
     }
 
-    setSquares(nextSquares)
-    setxIsNext(!xIsNext)
+    onPlay(nextSquares)
     
   }
   return (
@@ -56,7 +54,48 @@ function App() {
   );
 }
 
-export default App;
+export default function Game(){
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [xIsNext, setxIsNext] = useState(true)
+  const [currentMove, setCurrentMove] = useState(0)
+  const currentSquares = history[currentMove]
+  function handlePlay(nextSquares) {
+    
+    const nextHistory = [...history.slice(0,currentMove +1),nextSquares]
+    // setHistory([...history, nextSquares])
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
+    setxIsNext(!xIsNext)
+  }
+  function jumpTo(move){
+    setCurrentMove(move)
+    setxIsNext(move % 2 === 0)
+  }
+  const moves = history.map((squares,move)=>{
+    let description;
+    if (move > 0){
+      description = `Go to move #${move}`
+    }else{
+      description = 'Go to game start'
+    }
+    return(
+      <li key={move} className="transition-all hover:text-amber-800 hover:font-bold">
+        <button onClick={()=>jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return(
+    <div className="flex justify-center p-4">
+      <div className="mr-16">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="border border-gray-400 p-4">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+};
 
 function calculateWinner(squares) {
   const lines = [
